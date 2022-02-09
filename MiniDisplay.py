1aaee6cbfe250dad466a7a7631b5dceed3de3f6a
James Kirikland Garner <chip@garnertotalenergy.com>
2022-02-08 19:06:58 -0800

    Simplify MiniDisplay

--- MiniDisplay.py
+++ MiniDisplay.py
@@ -41,58 +41,33 @@ class MiniDisplay:
         
         # Get drawing object to draw on image.
         self.draw = ImageDraw.Draw(self.image)
-        
-        # Draw a black filled box to clear the image.
-        self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
-        
-        # Draw some shapes.
-        # First define some constants to allow easy resizing of shapes.
-        padding = -2
-        self.top = padding
-        bottom = self.height - padding
-        # Move left to right keeping track of the current x position for drawing shapes.
+
+        self.top = -2  # I don't know why.
         self.x = 0
-        
-        
-        # Load default font.
-        self.font = ImageFont.load_default()
-        
-        # Alternatively load a TTF font.  Make sure the .ttf font file is in the
+
+        self.font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 24)
+        # Load a TTF font.  Make sure the .ttf font file is in the
         # same directory as the python script!
         # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-        # font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
     
-    def loop_me(self):
+    def loop_me(self, text):
     
         # Draw a black filled box to clear the image.
         self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
-    
-        # Shell scripts for system monitoring from here:
-        # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-        cmd = "hostname -I | cut -d' ' -f1"
-        IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = 'cut -f 1 -d " " /proc/loadavg'
-        CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
-        MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
-        Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    
-        # Write four lines of text.
-    
-        self.draw.text((self.x, self.top + 0), "IP: " + IP, font=self.font, fill=255)
-        self.draw.text((self.x, self.top + 8), "CPU load: " + CPU, font=self.font, fill=255)
-        self.draw.text((self.x, self.top + 16), MemUsage, font=self.font, fill=255)
-        self.draw.text((self.x, self.top + 25), Disk, font=self.font, fill=255)
-    
-        # Display image.
+
+        self.draw.text((self.x, self.top), text, font=self.font, fill=255)
+
         self.disp.image(self.image)
         self.disp.show()
-        time.sleep(0.1)
 
 
 if __name__ == '__main__':
     display = MiniDisplay()
 
     while True:
-        display.loop_me()
+        cmd = 'cut -f 1 -d " " /proc/loadavg'
+        CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+        text = "CPU load: " + CPU
+        display.loop_me(text)
+        time.sleep(0.1)
