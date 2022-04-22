b0b6963fae1e6438f2c081a495a8837ebacc8579
James Kirikland Garner <chip@garnertotalenergy.com>
2022-04-22 11:31:13 +0800

    Time to ms

--- run_pms7003.py
+++ run_pms7003.py
@@ -55,7 +55,7 @@ class RunMePms7003:
             latest = self.sensors.get_latest()
             if self.display is not None:
                 self.display.display(latest)
-            time_stamped_latest = {"ts": time.time(), "values": latest}
+            time_stamped_latest = {"ts": round(time.time() * 1000), "values": latest}
             print(time_stamped_latest)
             self.publish.publish(time_stamped_latest)
 
